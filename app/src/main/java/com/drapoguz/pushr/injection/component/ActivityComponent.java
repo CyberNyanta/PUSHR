6fc39a18f40c766c7a7f98c62ae2624bf336fc0c
Eugene Drapoguz <drapoguz@gmail.com>
2017-03-26 11:03:00 +0300

    Renamed ActivityScope

--- app/src/main/java/com/drapoguz/pushr/injection/component/ActivityComponent.java
+++ app/src/main/java/com/drapoguz/pushr/injection/component/ActivityComponent.java
@@ -1,14 +1,14 @@
 package com.drapoguz.pushr.injection.component;
 
 import com.drapoguz.pushr.injection.module.ActivityModule;
-import com.drapoguz.pushr.injection.scope.PerActivity;
+import com.drapoguz.pushr.injection.scope.ActivityScope;
 import com.drapoguz.pushr.ui.main.MainActivity;
 import com.drapoguz.pushr.ui.main.personalrecords.MainPersonalRecordsFragment;
 import com.drapoguz.pushr.ui.training.TrainingActivity;
 
 import dagger.Component;
 
-@PerActivity
+@ActivityScope
 @Component(dependencies = AppComponent.class, modules = ActivityModule.class)
 public interface ActivityComponent {
 
